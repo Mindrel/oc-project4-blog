30f26fd9ff8837840623f83d114813815cc4ca75
Mindrel <juanmi66@gmail.com>
2020-06-29 22:12:31 +0200

    fix: tinymce encoding + <p> disable added

--- private/assets/js/tinymce.js
+++ private/assets/js/tinymce.js
@@ -1,9 +1,18 @@
 tinymce.init({
     selector: ".admin-textarea",
-    entity_encoding: "raw",
-    encoding: "UTF-8",
     language: "fr_FR",
     height: "360",
+
+    // Force l'encodage 
+    entity_encoding: "raw",
+    encoding: "UTF-8",
+
+    // Paramétrage des boutons et barres du champ de saisie
     menubar: "",
-    toolbar: "undo redo | bold italic underline | alignleft aligncenter alignright alignjustify | outdent indent | cut copy paste selectall"
+    toolbar: "undo redo | bold italic underline | alignleft aligncenter alignright alignjustify | outdent indent | cut copy paste selectall",
+
+    // Empêcher la création de paragraphes et remplacer par des sauts de lignes
+    forced_root_block: '',
+    force_br_newlines: true,
+    force_p_newlines: false
 });
