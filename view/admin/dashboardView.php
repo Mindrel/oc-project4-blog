42ffcb1e9c4aaf049548a8f4995c33e00577beea
Mindrel <juanmi66@gmail.com>
2020-07-04 17:20:53 +0200

    style: report number times minor change

--- view/admin/dashboardView.php
+++ view/admin/dashboardView.php
@@ -84,7 +84,7 @@
                     <?= htmlspecialchars($data["comment_extract"]) ?>
                 </td>
                 <td class="comment-counter-cell">
-                    <strong><?= $data["reporting_counter"] ?> fois</strong>
+                    <?= $data["reporting_counter"] ?> fois
                 </td>
                 <td class="comment-edit-cell">
                     <a href="index.php?action=commentEdit&id=<?= $data["id"] ?>"><i class="fas fa-edit"></i></a>
